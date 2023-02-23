7be500bb6003479983d966f2b1a9dffe094c2968
Luis Perez <44172765+Lap343@users.noreply.github.com>
2023-02-22 16:05:46 -0800

    feat(player-movement): add x and y movement

--- Assets/Scripts/PlayerControlles.cs
+++ Assets/Scripts/PlayerControlles.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerControlles : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
+    // [SerializeField] InputAction movement;
     [SerializeField] float movementSpeed = 0.1f;
 
     // Start is called before the first frame update
@@ -17,29 +17,33 @@ public class PlayerControlles : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // float xThrow = Input.GetAxis("Horizontal");
-        // float yThrow = Input.GetAxis("Vertical");
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
 
-        float xThrow = movement.ReadValue<Vector2>().x;
-        float yThrow = movement.ReadValue<Vector2>().y;
+        // float xThrow = movement.ReadValue<Vector2>().x;
+        // float yThrow = movement.ReadValue<Vector2>().y;
 
-        float newXPos = transform.localPosition.x + movementSpeed;
+        float xOffset = xThrow * Time.deltaTime * movementSpeed;
+        float newXPos = transform.localPosition.x + xOffset;
+
+        float yOffset = yThrow * Time.deltaTime * movementSpeed;
+        float newYPos = transform.localPosition.y + yOffset;
 
         transform.localPosition = new Vector3 (
             newXPos, 
-            transform.localPosition.y, 
+            newYPos, 
             transform.localPosition.z
         );
 
     }
 
-    void onEnable()
-    {
-        movement.Enable();
-    }
+    // void onEnable()
+    // {
+    //     movement.Enable();
+    // }
 
-    void onDisable()
-    {
-        movement.Disable();
-    }
+    // void onDisable()
+    // {
+    //     movement.Disable();
+    // }
 }
