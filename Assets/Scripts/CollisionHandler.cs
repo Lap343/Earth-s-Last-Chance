be537c8aa8a024d11f196d0fbff4a54f47296747
Luis Perez <44172765+Lap343@users.noreply.github.com>
2023-03-09 13:12:42 -0800

    feat(player-ship): add a reload level, stop particle system and disable player control  onTrigger

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,15 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other) 
-    {
-        Debug.Log(this.name + " bumped into " + other.gameObject.name);
-    }
+    [SerializeField] float loadLevelDelay = 2f;
+    [SerializeField] ParticleSystem leftLaser;
+    [SerializeField] ParticleSystem rightLaser;
+
+    bool isTransitioning = false;
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log(this.name + " bumped into " + other.gameObject.name);
+        if (isTransitioning) { return; }
+
+        StartCrashSequence();
+    }
+
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+        GetComponent<PlayerControlles>().enabled = false;
+        leftLaser.Stop();
+        rightLaser.Stop();
+        Invoke("ReloadLevel", loadLevelDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
